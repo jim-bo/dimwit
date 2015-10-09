607f8c285e418ec5ae148dae741a027e8439c068
James Lindsay <james@jamesrlindsay.com>
2015-10-09 00:23:38 -0400

    enabled cors

--- settings.py
+++ settings.py
@@ -4,8 +4,15 @@ MONGO_HOST = 'localhost'
 MONGO_PORT = 27017
 MONGO_DBNAME = 'dimwit'
 
-XML = False
+# We enable standard client cache directives for all resources exposed by the
+# API. We can always override these global settings later.
+CACHE_CONTROL = 'no-cache, no-store, must-revalidate'
+CACHE_EXPIRES = 0
+IF_MATCH = False
 
+# Allow CORS (Cross-Origin Resource Sharing)
+X_DOMAINS = '*'
+X_HEADERS = ['X-Requested-With', 'Authorization', 'Content-Type', 'Accept', 'If-Match', 'DELETE', 'Cache-Control']
 # Enable reads (GET), inserts (POST) and DELETE for resources/collections
 # (if you omit this line, the API will default to ['GET'] and provide
 # read-only access to the endpoint).
